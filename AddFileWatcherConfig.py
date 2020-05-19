80cd6b3b2a478549d38103233f3fe83fc2901674
devberutan <BERutan@NY5-76XW0S2-D>
2020-05-19 16:04:42 -0400

    Added AddFileWatcherConfig.py. Will serve as main script to generate FileWatcher xml schemas for new ETLs.

---
+++ AddFileWatcherConfig.py
@@ -0,0 +1,14 @@
+#####################################
+# AddFileWatcherConfig.py
+#####################################
+# Description:
+# * Using AddFileWatcherConfig.json, generate
+# new xml FileWatcher schema for QA/UAT/STG/PROD
+# paths, and append into FileWatcher configuration.
+
+
+def AddFileWatcherConfig():
+    pass
+
+if __name__ == '__main__':
+    AddFileWatcherConfig()
