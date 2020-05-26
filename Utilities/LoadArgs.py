3f51e5904d49a4d7729a77c4409d6ec99144e56f
devberutan <BERutan@NY5-76XW0S2-D>
2020-05-26 15:41:53 -0400

    Returning args instead of errs from ETLDashboardJsonArgs().

--- Utilities/LoadArgs.py
+++ Utilities/LoadArgs.py
@@ -101,7 +101,7 @@ def ETLDashboardJsonArgs():
         errs.append('(webapipath) Path must point to dll.')
     if errs:
         raise Exception('\n'.join(errs))
-    return errs
+    return args
 
 ############################
 # GenerateColumnAttributesReport.py
