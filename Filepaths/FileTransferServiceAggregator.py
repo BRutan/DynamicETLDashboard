ab8e6d5e1b2298156613b9808ac9846d5d3f4b02
devberutan <BERutan@NY5-76XW0S2-D>
2020-05-28 12:46:47 -0400

    Added Filepaths module which will deal with ETL filepaths. Added FleTransferServiceAggregator to open selenium and pull all RiskDashboard filetransferservice configurations for all etls, and output into json file.

---
+++ Filepaths/FileTransferServiceAggregator.py
@@ -0,0 +1,49 @@
+#####################################
+# FileTransferServiceAggregator.py
+#####################################
+# Description:
+# * Aggregate all filepaths from
+# FileTransferService to allow
+# fast lookup for data source and output
+# paths.
+
+import selenium
+
+class FileTransferServiceAggregator:
+    """
+    * Aggregate all filepaths from FileTransferService to allow
+    fast lookup for data source and output paths.
+    """
+    def __init__(self, ftsserver, grouping):
+        """
+        * Open Selenium instance and aggregate
+        all filetransfers.
+        """
+        FileTransferServiceAggregator.__Validate(ftsserver, grouping)
+        self.__transfersjson = {}
+        self.__PullFromFTS()
+
+    ####################
+    # Interface Methods:
+    ####################
+    def OutputLookup(self):
+        """
+        * 
+        """
+        pass
+
+    ####################
+    # Private Helpers:
+    #################### 
+    def __PullFromFTS(self):
+        """
+        * Open Chrome instance with Selenium to pull all existing
+        filetransferservice paths.
+        """
+        try:
+            runSelenium = None
+        except Exception as ex:
+            errs.append('Missing chromedriver from PATH.')
+            errs.append('Please download from https://sites.google.com/a/chromium.org/chromedriver/downloads')
+            errs.append('and add to PATH variable.')
+            raise Exception('\n'.join(errs))
