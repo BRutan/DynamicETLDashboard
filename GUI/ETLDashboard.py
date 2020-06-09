017d5b825eb014b4057fbdf3c02cf2d0da0307e7
devberutan <BERutan@NY5-76XW0S2-D>
2020-06-09 17:10:47 -0400

    Updated the GUI.

--- GUI/ETLDashboard.py
+++ GUI/ETLDashboard.py
@@ -5,11 +5,10 @@
 # * GUI for ETL dashboard, contains all sub
 # windows.
 
-
 from tkinter import Tk
 from tkinter.ttk import Notebook
 from GUI.ETLInfoWindow import ETLInfoWindow
-from GUI.ETLInfoWindow import ETLInfoWindow
+from GUI.TestETLPipelineWindow import TestETLPipelineWindow
 
 class ETLDashboard(object):
     """
@@ -18,7 +17,7 @@ class ETLDashboard(object):
     def __init__(self, windows):
         self.__etldashboard = Tk("ETLDashboard", "ETLDashboard", "ETLDashboard")
         self.__windows = Notebook(self.__etldashboard)
-        self.__testetlpipelinewindow = None
+        self.__testetlpipelinewindow = TestETLPipelineWindow()
         self.__etlinfowindow  = ETLInfoWindow()
 
         self.__window.title("ETLDashboard")
--- GUI/TestETLPipelineWindow.py
+++ GUI/TestETLPipelineWindow.py
@@ -1,1 +1,17 @@
+#####################################
+# TestETLPipelineWindow.py
+#####################################
+# Description:
+# * 
 
+from GUI.ScriptWindow import ScriptWindow
+
+class TestETLPipelineWindow(ScriptWindow):
+    """
+    * 
+    """
+    def __init__(self):
+        """
+        * 
+        """
+        super().__init__("TestETLPipeline")
